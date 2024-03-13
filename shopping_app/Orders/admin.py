3d6e25ec7585d74dcf704c828bdcb10d9f7bf50b
Dheeraj Singh <dheerajc991@gmail.com>
2024-03-13 18:04:51 +0530

    project setup

--- shopping_app/Orders/admin.py
+++ shopping_app/Orders/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from shopping_app.Orders.models import orders
 # Register your models here.
+admin.site.register(orders)
--- shopping_app/Orders/models.py
+++ shopping_app/Orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class orders(models.Model):
+    order_id = models.CharField(max_length=100)
+    order_date = models.DateField()
+    order_status = models.CharField(max_length=100)
+    order_total = models.IntegerField()
+    order_customer = models.CharField(max_length=100)
+    order_product = models.CharField(max_length=100)
+    order_quantity = models.IntegerField()
+    order_price = models.IntegerField()
